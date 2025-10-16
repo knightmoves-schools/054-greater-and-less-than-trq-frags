15f7f492a91477fa281ebad08c6c098af5082c77
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-16 18:14:36 +0000

    hjjjjjjjjjjjjklserlgsrehsdrghrgsrgr

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+if(18 > 17){
+    document.getElementById("result").innerHTML = 'oldEnough';
+}
 
+if(17 >= 17){
+    document.getElementById("result").innerHTML = 'underAge';
+}
+
+if(50 < 65){
+    document.getElementById("result").innerHTML = 'legalSpeed';
+}
+
+if(65 <= 65){
+    document.getElementById("result").innerHTML = 'isGoldenYears';
+}
